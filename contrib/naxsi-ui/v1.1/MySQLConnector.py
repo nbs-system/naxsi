e53155c97382203160de1a70ace595f7bac7b627
sbz <sbz@6dev.net>
2012-03-29 12:30:22 +0000

    use ini format and configparser builtin

--- contrib/naxsi-ui/v1.1/MySQLConnector.py
+++ contrib/naxsi-ui/v1.1/MySQLConnector.py
@@ -1,20 +1,26 @@
 import MySQLdb
 
+from ConfigParser import ConfigParser
+
+class MySQLConnectorException(Exception)
+    pass
+
 class MySQLConnector(object):
-    def __init__(self, filename = './sql_id'):
+    section_name = 'mysql'
+    def __init__(self, filename = 'naxsi-ui.conf'):
         try:
             fd = open(filename, 'r')
         except:
-            print 'Cannot open file %s' % filename
-        for i in fd:
-            if i.startswith('user='):
-                self.user = i[5:].strip()
-            if i.startswith('pass='):
-                self.password = i[5:].strip()
-            if i.startswith('host='):
-                self.host = i[5:].strip()
-            if i.startswith('name='):
-                self.dbname = i[5:].strip()
+            raise MySQLConnectorException('Cannot open file %s' % filename)
+        self.conf = ConfigParser()
+        self.conf.readfp(fd)
+        self.user = self.get_config('username')
+        self.host = self.get_config('hostname')
+        self.password = self.get_config('password')
+        self.dbname = self.get_config('dbname')
+
+    def get_config(self, key):
+        return self.conf.get(self.__class__.section_name, key)
 
     def connect(self):
         return MySQLdb.connect(self.host, self.user, self.password, self.dbname)

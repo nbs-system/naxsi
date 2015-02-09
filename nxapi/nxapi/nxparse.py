dc5f5be05d31f0c746a5a57bd078478ccf262ce1
Santiago Rodriguez <santi@trisquel.info>
2015-02-09 16:36:03 +0100

    Add --fmt-only option to nxtool.py
    Avoids feeding EXLOG entries to elasticsearch

--- nxapi/nxapi/nxparse.py
+++ nxapi/nxapi/nxparse.py
@@ -122,14 +122,18 @@ class NxReader():
 
 
 class NxParser():
-    def __init__(self):
+    def __init__(self,fmt_only=False):
         # output date format
         self.out_date_format = "%Y/%m/%d %H:%M:%S"
         # Start of Data / End of data marker
         self.sod_marker = [' [error] ', ' [debug] ']
         self.eod_marker = [', client: ', '']
         # naxsi data keywords
-        self.naxsi_keywords = [" NAXSI_FMT: ", " NAXSI_EXLOG: "]
+	if fmt_only is not False:
+                self.naxsi_keywords = [" NAXSI_FMT: "]
+                print "Parsing only NAXSI_FMT entries"
+        else:
+                self.naxsi_keywords = [" NAXSI_FMT: ", " NAXSI_EXLOG: "]
         # keep track of fragmented lines (seed_start=X seed_end=X)
         self.reunited_lines = 0
         self.fragmented_lines = 0

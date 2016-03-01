105fffd1dc185c3412bb117e9c87c610d95d6182
bui <bui@nbs-system.com>
2016-03-01 10:20:40 +0100

    update copyright \o/

--- naxsi_src/naxsi_json.c
+++ naxsi_src/naxsi_json.c
@@ -1,6 +1,6 @@
 /*
  * NAXSI, a web application firewall for NGINX
- * Copyright (C) 2011, Thibault 'bui' Koechlin
+ * Copyright (C) 2016, Thibault 'bui' Koechlin
  *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

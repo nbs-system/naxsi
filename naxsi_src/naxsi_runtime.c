2ea00ad005fb0bd862ef7a018f879141a0bd10da
Manuel <manuel.sabban@nbs-system.com>
2018-11-13 11:28:28 +0100

    should fix 447

--- naxsi_src/naxsi_runtime.c
+++ naxsi_src/naxsi_runtime.c
@@ -809,7 +809,8 @@ ngx_int_t ngx_http_nx_log(ngx_http_request_ctx_t *ctx,
   const char 	*fmt_base = "ip=%.*s&server=%.*s&uri=%.*s&vers=%.*s&total_processed=%zu&total_blocked=%zu&config=%.*s";
   const char	*fmt_score = "&cscore%d=%.*s&score%d=%zu";
   const char	*fmt_rm = "&zone%d=%s&id%d=%d&var_name%d=%.*s";
-  const char    *fmt_config =  ctx->learning ? (ctx->drop ? "learning-drop" : "learning" ) :  (ctx->drop ? "drop" : (ctx->block ? "block" : ""));
+  const char    *fmt_config =  ctx-> log ? (ctx->learning ? (ctx->drop ? "log-learning-drop" : "log-learning" ) :  (ctx->drop ? "log-drop" : (ctx->block ? "log-block" : ""))) : \
+    (ctx->learning ? (ctx->drop ? "learning-drop" : "learning" ) :  (ctx->drop ? "drop" : (ctx->block ? "block" : "")));
 
   ngx_http_dummy_loc_conf_t	*cf;
   ngx_http_matched_rule_t	*mr;
